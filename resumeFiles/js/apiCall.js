585b0870146a58f92d106817a634fb1036019e27
FAYZERJFL <109256788+FAYZERJFL@users.noreply.github.com>
2022-11-26 20:55:30 -0700

    Delete resumeFiles/js directory

--- resumeFiles/js/apiCall.js
+++
@@ -1,9 +0,0 @@
-var requestOptions = {
-  method: 'POST',
-  redirect: 'follow'
-};
-
-fetch("https://usigzt2y68.execute-api.us-east-1.amazonaws.com/default/fayzerVisitorCounter", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
