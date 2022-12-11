3b5801041450ded1d0b9e3c91557416e487be6bb
FAYZERJFL <109256788+FAYZERJFL@users.noreply.github.com>
2022-12-11 13:53:16 -0700

    VC FINALLY HAS NO CURLY BRACES OR QUOTES!!!!!!!!!

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
 var xhr = new XMLHttpRequest();
+
   xhr.withCredentials = false;
   xhr.addEventListener("readystatechange", function() {
+
     if(this.readyState === 4) {
-      document.getElementById('viewCount').innerHTML = (this.responseText);
-      console.log(this.responseText);
+
+      var visitors = `Site visitors: ${JSON.stringify(this.responseText)}`;
+      
+        document.getElementById('viewCount').innerHTML = visitors;
+
+      var output = document.getElementById('viewCount').innerHTML;
+      var result = output.replace(/"/g, '');
+
+        document.getElementById('viewCount').innerHTML = result;
+
+        console.log(result);
     }
   });
+
 xhr.open("POST", "https://usigzt2y68.execute-api.us-east-1.amazonaws.com/default/fayzerVisitorCounter");
 xhr.send();
+
+
+
+
+
+
 
 
 window.onload = (function() {
   setTimeout(function() {
     $('#exampleModal').modal('show');
-  }, 10000);
+  }, 5000);
 });
+
+
