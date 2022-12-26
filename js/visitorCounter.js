980efd46e72f5a6d8ffbb75754c84351443403fd
FAYZERJFL <109256788+FAYZERJFL@users.noreply.github.com>
2022-12-26 14:58:49 -0700

    efqee

--- js/visitorCounter.js
+++ js/visitorCounter.js
@@ -1,11 +1,11 @@
 let xhr = new XMLHttpRequest();
-  xhr.withCredentials = false;
-  xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-      let visitors = (`~Site visitors: ${JSON.stringify(this.responseText)}~`).replace(/"/g, '');
-      document.getElementById('viewCount').innerHTML = visitors;
-      console.log(visitors);
-    }
-  });
+xhr.withCredentials = false;
+xhr.addEventListener("readystatechange", function(){
+  if(this.readyState === 4){
+    let visitors = (`~Site visitors: ${JSON.stringify(this.responseText)}~`).replace(/"/g, '');
+    document.getElementById('viewCount').innerHTML = visitors;
+    console.log(visitors);
+  }
+});
 xhr.open("POST", "https://293gl3o0bk.execute-api.us-east-1.amazonaws.com/default/fayzerVisitorCounter");
 xhr.send();
