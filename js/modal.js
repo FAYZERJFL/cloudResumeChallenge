5d32fcf1dab50bbcb354dccbc8de8656a8c0b4c2
FAYZERJFL <109256788+FAYZERJFL@users.noreply.github.com>
2022-12-26 14:25:56 -0700

    NTAENAAENT

--- js/modal.js
+++ js/modal.js
@@ -1,44 +1,34 @@
 window.onload = (function() {
   setTimeout(function() {
     $('#modal').modal('show');
-  }, 7000);
+  }, 200);
+
+  const infoSubmit = document.getElementById("submit-button");
+  const apiSend = function() {
+      const visitorName = document.getElementsByTagName("input")[0].value;
+      const visitorEmail = document.getElementsByTagName("input")[1].value;
+      const myHeaders = new Headers();
+      const dateTime = new Date();
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify({
+          name:visitorName,
+          email:visitorEmail,
+          time:dateTime,
+        }),
+        redirect: 'follow',
+      };
+      
+      const request = new Request("https://7i3xd8khgh.execute-api.us-east-1.amazonaws.com/default/fayzerVisitorInfo", requestOptions);
+
+      fetch(request)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+      
+    };
+
+  infoSubmit.addEventListener('click', apiSend);
+  
 });
-
-
-const infoSubmit = document.getElementById("submit-button");
-
-const apiSend = function(){
-
-  const visitorName = document.getElementsByTagName("input")[0].value;
-  const visitorEmail = document.getElementsByTagName("input")[1].value;
-    console.log(visitorName);
-    console.log(visitorEmail);
-    
-  var formdata = new FormData();
-    formdata.append("name: ", visitorName);
-    formdata.append("email: ", visitorEmail);
-
-  console.log([...formdata]);
-
-  
-
-
-  var myHeaders = new Headers({'Content-Type':'application/x-www-form-urlencoded'});
-    myHeaders.append("Access-Control-Allow-Headers", "Content-Type");
-    myHeaders.append("Access-Control-Allow-Origin", "*");
-    myHeaders.append("Access-Control-Allow-Methods", "POST, OPTIONS");
-  
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: formdata,
-    redirect: 'follow'
-  }
-
-  fetch("https://iot46sut9k.execute-api.us-east-1.amazonaws.com/default/fayzerVisitorInfo", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-}; 
-
-infoSubmit.addEventListener('click', apiSend);
